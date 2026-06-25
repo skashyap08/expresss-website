46dc78c2a69dd201c1b3db6d442df27d2715c566
skashyap08 <sakshikashyap9876@gmail.com>
2026-06-25 14:12:53 +0530

    file updated-2

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const path = require('path');
 
 const app = express();
 const PORT = 3000;
+// Static Files
+app.use(express.static('public'));
+
+// Routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'home.html'));
+});
